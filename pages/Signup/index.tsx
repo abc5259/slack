6dcf989bebfdbbdfb3c966915d26d3dfea60d82b
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-10 02:45:17 +0900

    feat: working Login Page

--- pages/Signup/index.tsx
+++ pages/Signup/index.tsx
@@ -1,6 +1,7 @@
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from './styles';
 
 const SignUp = () => {
@@ -90,7 +91,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
   );
